b29d834e2e42a05ed20b155e9b71b8b5307c9f6c
Blons Pierrick <pierrick@sail.cloud>
2018-07-10 13:28:50 +0200

    add: method ToWords()

--- SQLI.KataNumbersInWords/KataNumbers.cs
+++ SQLI.KataNumbersInWords/KataNumbers.cs
@@ -4,5 +4,9 @@ namespace SQLI.KataNumbersInWords
 {
     public class KataNumbers
     {
+        public static string ToWords(object p)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
