e30483575023e1befa4383acb3fdecc6b4917c58
Blons Pierrick <pierrick@sail.cloud>
2018-07-10 13:52:27 +0200

    refac: Using conversiontable dictionnary to retrieve corresponding string of param

--- SQLI.KataNumbersInWords/KataNumbers.cs
+++ SQLI.KataNumbersInWords/KataNumbers.cs
@@ -1,22 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SQLI.KataNumbersInWords
 {
     public class KataNumbers
     {
+        static Dictionary<string,string> _conversionTable = new Dictionary<string, string>()
+        { 
+            { "1", "One"},
+            { "12", "Twelve"} 
+        };
+
         public static string ToWords(string number)
         {
             if(number == default(string)){
                 return string.Empty;
             }
 
-            if (number == "1")
-                return "One";
-
-            if (number == "12")
-                return "Twelve";
-            
-            throw new NotImplementedException();
+            _conversionTable.TryGetValue(number, out string result);
+            return result;
         }
     }
 }
